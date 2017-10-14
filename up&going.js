e525fb49dc1f5a5c92e985f80db3f2d1b7e0e45a
jonay <casa@gmail.com>
2017-10-14 11:49:12 +0100

    first commit, first test

---
+++ up&going.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let should = require('chai').should();
+let assert = require('chai').assert;
+let expect = require('chai').expect;
+
+
+describe("Up & Going", () => {
+    it('green test', function () {
+        true.should.equal(true);
+    });
+});
